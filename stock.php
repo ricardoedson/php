b195fde4e68ba3d70e1fc8c457103ec027bb5f0f
ricardoedson <edsotz2018@gmail.com>
2023-09-05 22:30:20 -0500

    cambios

---
+++ stock.php
@@ -0,0 +1,20 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $stock = 800;
+    //si el stock es mayor que 0, entonces;
+    //imprime "hay stock"
+    //sino
+    //no hay stock
+
+    if ($stock > 0)
+    {
+        echo "Hay stock";
+    }
+    else 
+    {
+        echo "No hay stock";
+    }
+?>
