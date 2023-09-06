0bdf1ad0db356449e2b71eaf06657302751ed2e9
ricardoedson <edsotz2018@gmail.com>
2023-09-06 14:56:06 -0500

    nuevo

---
+++ listado_productos.php
@@ -0,0 +1,9 @@
+<?php
+
+    //Mostrar en pantalla un listado de productos indicando si hay poco stock (<=10), sin stock (0) o hay stock(>10)
+    //de electrodomesticos
+
+    
+
+
+?>
---
+++ rand.php
@@ -0,0 +1,31 @@
+<?php
+
+    #la fn rand(x,y) genera un numero aleatorio dentro del rango x e y
+
+    $valor = rand(1,5);
+
+    #si el num generado 1, 3, 5 mostrar en pantalla sino mostrar el numero es par 
+    
+    
+    // && = Y
+    // || = O
+
+    /*if ($valor==1 || $valor==3 || $valor==5)
+    {
+        echo "el número $valor es impar";
+    } else {
+        echo "el número $valor es par";
+    }*/
+
+    if ($valor %2==0)
+    {
+        echo "el número $valor es par";
+    } else {
+        echo "el número $valor es impar";
+    }
+
+    echo $valor==1 || $valor==3 || $valor==5? "El numero $valor es impar" : "El numero $valor es par";
+
+
+
+?>
