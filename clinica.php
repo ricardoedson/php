de59b31b5554a477258695097551a7eea0d83b57
ricardoedson <edsotz2018@gmail.com>
2023-09-07 14:01:04 -0500

    cambios

--- clinica.php
+++ clinica.php
@@ -3,25 +3,55 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$aPacientes = array();
+$aPacientes[] = array(
+    "dni" => "33.765.012",
+    "nombreApellido" => "Ana Acuña",
+    "edad" => 45,
+    "peso" => 81.5,
+    );
+$aPacientes[] = array(
+    "dni" => "23.684.385",
+    "nombreApellido" => "Gonzalo Bustamante",
+    "edad" => 66,
+    "peso" => 79,
+    ) ;
+$aPacientes[] = array(
+        "dni" => "23.684.386",
+        "nombreApellido" => "Juan Irraola",
+        "edad" => 28,
+        "peso" => 79,
+        ) ;
+$aPacientes[] = array(
+        "dni" => "23.684.387",
+        "nombreApellido" => "Beatriz Ocampo",
+        "edad" => 50,
+        "peso" => 80,
+            ) ;
+
+    //var_dump($aPacientes);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <title>Clinica Nutricion S.A</title>
 </head>
 <body>
     <main class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 text-center py-5">
                 <h1>Listado de pacientes</h1>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
-                <table class="table table-hover border">
+                <table class="table table-hover border my-2">
                     <thead>
                         <th>DNI</th>
                         <th>Nombre y apellido</th>
@@ -29,7 +59,17 @@ error_reporting(E_ALL);
                         <th>Peso</th>
                     </thead>
                     <tbody>
-                        <td></td>
+                        <?php
+
+                        for ($cont=0; $cont < count($aPacientes); $cont++){
+                        echo "<tr>";
+                        echo "<td>". $aPacientes[$cont]["dni"] . "</td>";
+                        echo "<td>". $aPacientes[$cont]["nombreApellido"] . "</td>";
+                        echo "<td>". $aPacientes[$cont]["edad"] . "</td>";
+                        echo "<td>". $aPacientes[$cont]["peso"] . "</td>";
+                        echo "</tr>";
+                        }
+                        ?>
                     </tbody>
                 </table>
             </div>
