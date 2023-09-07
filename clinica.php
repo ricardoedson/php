ff740bc62ea5b9e3f5b063aa82d8219d54859a2b
ricardoedson <edsotz2018@gmail.com>
2023-09-07 12:28:35 -0500

    nuevo

---
+++ clinica.php
@@ -0,0 +1,39 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clinica Nutricion S.A</title>
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Listado de pacientes</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <thead>
+                        <th>DNI</th>
+                        <th>Nombre y apellido</th>
+                        <th>Edad</th>
+                        <th>Peso</th>
+                    </thead>
+                    <tbody>
+                        <td></td>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
