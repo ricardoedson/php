eefbb9f44d341645c91c1473491bf14caa85974c
ricardoedson <edsotz2018@gmail.com>
2023-09-08 15:44:20 -0500

    cambios

--- pruebas_foreach.php
+++ pruebas_foreach.php
@@ -3,4 +3,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$miAuto = array();
+$miAuto = array(
+    "patente" => "AA123HB",
+    "marca" => "Ford",
+);
+
+foreach($miAuto as $clave => $valor){
+    echo "La $clave es $valor <br>";
+}
 ?>
