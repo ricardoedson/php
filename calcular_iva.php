4a32233415150bdcd32ee57c59b8d95e5a5f7508
ricardoedson <edsotz2018@gmail.com>
2023-10-11 21:59:32 -0500

    cambios

--- calcular_iva.php
+++ calcular_iva.php
@@ -3,7 +3,28 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$iva = 19;
+$precioConIva=0;
+$precioSinIva=0;
+$ivaCantidad=0;
 
+if($_POST){
+    $iva = $_POST ["lstIva"];       //variable global php   
+    $precioConIva = ($_POST ["txtPrecioConIva"]) > 0? $_POST ["txtPrecioConIva"]: 0;
+    $precioSinIva = ($_POST ["txtPrecioSinIva"]) > 0? $_POST ["txtPrecioSinIva"]: 0;
+
+    //Dado un importe sin iva, Precio con IVA = importe * (21/100+1)
+    if ($precioSinIva > 0) {
+         $precioConIva = $precioSinIva * ($iva/100+1);
+    }
+
+    //Dado un importe con IVA, Precio sin IVA = importe / (21/100+1) (Caso del consumidor final)
+    if ($precioConIva > 0) {
+        $precioSinIva = $precioConIva / ($iva/100+1);
+   }
+
+    $ivaCantidad = $precioConIva - $precioSinIva;
+}
 
 ?>
 
@@ -19,25 +40,25 @@ error_reporting(E_ALL);
 <body>
     <main class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 text-center py-5">
                 <h1>Calculadora de IVA</h1>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-6 py-5">
+            <div class="col-3 offset-2 py-5">
                 <form action="" method="post">
-                    <div>
+                    <div class="pb-3">
                         <label for="">IVA</label>
-                        <select name="lstIva" id="lstIva" class="form-control">
+                        <select name="lstIva" id="lstIva" class="form-control">     <!--name: para php(backend), y id para frontend-->
                             <option value="10.5">10.5</option>
-                            <option value="19">19</option>
+                            <option value="19" selected>19</option>
                             <option value="21">21</option>
                         </select>
                     </div>
-                    <div>
+                    <div class="pb-3">
                         <label for="">Precio sin IVA</label>
-                        <input type="text" name="txtPrecionSinIva" id="txtPrecioSinIva" class="form-control">
+                        <input type="text" name="txtPrecioSinIva" id="txtPrecioSinIva" class="form-control">
                     </div>       
                     <div class="pb-3">
                         <label for="">Precio con IVA</label>
@@ -48,23 +69,23 @@ error_reporting(E_ALL);
                     </div>
                 </form>
             </div>
-            <div class="col-6 py-5">
+            <div class="col-5 pt-5 mt-4">
                 <table class="table table-hover border">
                     <tr>
                         <th>IVA:</th>
-                        <td>%</td>
+                        <td><?php echo $iva;  ?>%</td>
                     </tr>
                     <tr>
                         <th>Precio sin IVA:</th>
-                        <td>$</td>
+                        <td>$ <?php echo number_format($precioSinIva, 2, ",", ".");  ?></td>
                     </tr>
                     <tr>
                         <th>Precio con IVA:</th>
-                        <td>$</td>
+                        <td>$ <?php echo number_format($precioConIva, 2, ",", ".");  ?></td>
                     </tr>
                     <tr>
                         <th>IVA cantidad:</th>
-                        <td>$</td>
+                        <td>$ <?php echo number_format($ivaCantidad, 2, ",", ".");  ?></td>
                     </tr>
                 </table>
             </div>
