e13a9fdb83c77429657bd3f9563c9095eea0877e
ricardoedson <edsotz2018@gmail.com>
2023-10-18 15:07:41 -0500

    cambios

--- clientes_session.php
+++ clientes_session.php
@@ -13,8 +13,11 @@ if(isset($_SESSION["listadoClientes"])){
     $aClientes = array();
 }
 
-print_r($_SESSION);
+//print_r($_SESSION);
 if($_POST){
+    print_r($_POST);
+    //si hace click en enviar, entonces: 
+    if(isset($_POST["btnEnviar"])){
     //asignamos variables de los datos del formulario que viene
     $nombre = $_POST["txtNombre"];
     $dni = $_POST["txtDni"];
@@ -28,6 +31,19 @@ if($_POST){
         );
                     //actualizar el contenido de variable de session
                     $_SESSION["listadoClientes"] = $aClientes;
+
+    }
+
+    //si hace click en eliminar, entonces:
+    if(isset($_POST["btnEliminar"])){
+    //session_destroy();
+        session_destroy();      //elimina session pero no borra datos del array:
+        $aClientes = array();  //sobreescribe el valor del array a vacio para no dar click eliminar 2 veces
+    }
+
+
+
+    //Agregar para eliminar una fila
     
 }
 
@@ -41,6 +57,7 @@ if($_POST){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
     <title>Tabla Clientes</title>
 </head>
@@ -79,14 +96,20 @@ if($_POST){
                         <th>DNI</th>
                         <th>Teléfono</th>
                         <th>Edad</th>
+                        <th></th>
                     </thead>
                     <tbody>
-                    <?php foreach ($aClientes as $cliente): ?>
+                        <?php foreach ($aClientes as $pos => $cliente): ?>
                             <tr>
                                 <td><?php echo $cliente["nombre"]; ?></td>
                                 <td><?php echo $cliente["dni"]; ?></td>
                                 <td><?php echo $cliente["telefono"]; ?></td>
                                 <td><?php echo $cliente["edad"]; ?></td>
+                                <td>
+                                    <form action="" method="POST">
+                                        <button type="submit" name="btnEliminar_<?php echo $pos; ?>" class="btn btn-outline-warning"><i class="bi bi-trash"></i></button>  
+                                    </form>  
+                                </td>
                             </tr>
                         <?php endforeach; ?>
                         
